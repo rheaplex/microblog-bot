f8117f2dacc5aaaf399702d47aa35ac151c7ea61
Rob Myers <robmyers@rob-laptop.(none)>
2009-06-03 22:00:48 +0100

    Add install helper script. sbcl-specific.

---
+++ install.lisp
@@ -0,0 +1,26 @@
+;; install.lisp -  Run once to install support libraries for building.
+;; Copyright (C) 2009  Rob Myers rob@robmyers.org
+;;
+;; This file is part of microblog-bot.
+;;
+;; microblog-bot is free software; you can redistribute it and/or 
+;; modify it under the terms of the GNU General Public License as published 
+;; by the Free Software Foundation; either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; microblog-bot is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(require 'asdf)
+(require 'asdf-install)
+
+(asdf-install:install 'cxml-stp)
+(asdf-install:install 'drakma)
+
+(when (not (file-probe "./cl-twit.asd"))
+  (sb-posix:symlink "./cl-twit/cl-twit.asd" "./cl-twit.asd"))
