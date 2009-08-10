771f25af46e4e46f77a782a5bca62a03495be59d
Rob Myers <robmyers@robs-laptop.(none)>
2009-08-10 21:04:28 +0100

    More robust error handling.

--- microblog-bot.lisp
+++ microblog-bot.lisp
@@ -44,6 +44,8 @@
 			   :initarg :most-recent-mention-id 
 			   :initform 0)))
 
+(defvar *live* nil)
+
 (defmacro with-microblog-user (user &body body)
   "Log in, execture the body, log out."
   `(progn
@@ -53,6 +55,25 @@
        ,@body)
      ;; with-session doesn't currently do this, remove if it ever does.
      (cl-twit:logout)))
+
+(defmacro update-id (place item)
+  "Change the id at place for item's id if higher"
+  (let ((new-id (gensym)))
+    `(let ((,new-id (cl-twit::id ,item))) 
+       (when (> ,new-id ,place)
+	 (seft ,place ,new-id)))))
+
+(defun set-debug ()
+  (setf *live* nil)
+  (setf (symbol-function 'post) #'(lambda(message)
+				    (format t "~a~%" message))))
+
+(defun set-live ()
+  (setf *live* t)
+  (setf (symbol-function 'post) #'(lambda (message)
+	       (cl-twit:update message))))
+
+(set-live)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Basic bot
@@ -78,7 +99,7 @@
 		 :initform 0)
    (min-wait :accessor min-wait 
 	     :initarg :min-wait
-	     :initform (* 60 10))
+	     :initform (* 60 60 1))
    (max-wait :accessor max-wait 
 	     :initarg :max-wait 
 	     :initform (* 60 60 4))
@@ -100,30 +121,48 @@
 	   (random (- (max-wait bot)
 		      (min-wait bot))))))
 
+(defun current-user-replies-after-id ()
+  "Get the exclusive lower bound for replies to the user to check."
+  ;; Wasteful, as we only want the most recent id
+  ;; If the user hasn't posted yet, start with the most recent global id
+  (or (cl-twit::get-newest-id (cl-twit:m-replies))
+      (cl-twit::get-newest-id (cl-twit:m-public-timeline))))
+
 (defmethod initialize-instance :after ((bot microblog-bot) &key)
   "Set up the bot's state."
   (assert (source-url bot))
   (with-microblog-user bot
     (setf (previous-day bot) (floor (get-universal-time) %one-day))
     (when (= (most-recent-mention-id bot) 0)
-      ;; Wasteful, as we only want the most recent id
-      ;; If the user hasn't posted yet, start with the most recent global id
       (setf (most-recent-mention-id bot)
-	    (or (cl-twit::get-newest-id (cl-twit:m-replies))
-		(cl-twit::get-newest-id (cl-twit:m-public-timeline)))))))
+	    (current-user-replies-after-id)))))
+
+(defmethod response-for-source-request ((bot microblog-bot) mention)
+  "Response for the source request."
+  (format nil "@~a Hi! You can get my source here: ~a" 
+	  (cl-twit:user-screen-name 
+	   (cl-twit:status-user mention))
+	  (source-url bot)))
 
 (defmethod respond-to-source-request ((bot microblog-bot) mention)
   "Respond to the source request."
-  (cl-twit:update (format nil "@~a Hi! You can get my source here: ~a" 
-			  (cl-twit:user-screen-name 
-			   (cl-twit:status-user mention))
-			  (source-url bot))))
+  (let ((result (ignore-errors 
+		  (cl-twit:update (response-for-source-request bot mention)))))
+    (when result
+      (update-id (most-recent-mention-id bot) result))))
+
+(defmethod response-for-mention ((bot microblog-bot) mention)
+  "Response for the mention object."
+  (format nil "@~a Hi!" 
+	  (cl-twit:user-screen-name 
+	   (cl-twit:status-user mention))))
 
 (defmethod respond-to-mention ((bot microblog-bot) mention)
   "Respond to the mention object in some way."
-  (cl-twit:update (format nil "@~a Hi!" 
-			  (cl-twit:user-screen-name 
-			   (cl-twit:status-user mention)))))
+  (let ((result (ignore-errors 
+		  (cl-twit:update (response-for-mention bot mention)))))
+    (when result
+      (update-id (most-recent-mention-id bot) result))))
 
 (defmethod filter-mentions ((bot microblog-bot) mentions)
   "Make sure only one mention from each user is listed."
@@ -145,13 +184,10 @@
 			   (cl-twit:status-user mention))
 			 (ignore-messages-from bot)
 			 :test #'string=))
-	  (handler-case 
+	  (ignore-errors 
 	      (if (search "!source" (cl-twit:status-text mention))
 		  (respond-to-source-request bot mention)
-		  (respond-to-mention bot mention))
-	    (cl-twit:twitter-error () nil))))
-      (setf (most-recent-mention-id bot)
-	    (cl-twit::get-newest-id replies)))))
+		  (respond-to-mention bot mention))))))))
 
 (defmethod daily-task ((bot microblog-bot))
   "Performed every day at midnight."
@@ -162,12 +198,14 @@
   (multiple-value-bind (days seconds) (floor (get-universal-time) %one-day)
     (when (and (> days (previous-day bot))
 	       (> seconds (* (daily-task-hour bot) %one-hour)))
-      (daily-task bot)
-      (setf (previous-day bot) days))))
+      (ignore-errors
+	(daily-task bot)
+	;; Only update if successful
+	;; Is this OK?
+	(setf (previous-day bot) days)))))
 
 (defmethod constant-task ((bot microblog-bot))
   "Performed every time the bot wakes up."
-  (format t "Once~%")
   nil)
 
 (defmethod intermittent-task ((bot microblog-bot))
@@ -178,27 +216,25 @@
   "Run the task or update the wait time."  
   (if (<= (wait-remaining bot) 0)
       (progn
-	(handler-case
-	    (intermittent-task bot)
-	  (cl-twit:twitter-error () nil))
+	(ignore-errors
+	    (intermittent-task bot))
 	(reset-wait bot)))
   (setf (wait-remaining bot)
 	(- (wait-remaining bot)
 	   *bot-sleep-time*)))
 
 (defmethod run-bot-once ((bot microblog-bot))
-  (with-microblog-user bot
-    (manage-daily-task bot)
-    (constant-task bot)
-    (respond-to-mentions bot)
-    (manage-intermittent-task bot)))
+  (ignore-errors
+       (with-microblog-user bot
+	 (manage-daily-task bot)
+	 (constant-task bot)
+	 (respond-to-mentions bot)
+	 (manage-intermittent-task bot))))
 
 (defmethod run-bot ((bot microblog-bot))
   "Loop forever responding to mentions & occasionaly performing periodic-task."
   (loop 
-     (handler-case
-	 (run-bot-once bot)
-       (cl-twit:twitter-error () nil))
+     (run-bot-once bot)
      (sleep *bot-sleep-time*)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -212,20 +248,29 @@
    (follow-id :accessor follow-id 
 	      :initarg :follow-id)))
 
+(defun current-user-posts-after-id (bot)
+  "Get the exclusive lower bound for replies to the user to check."
+  ;; Wasteful, as we only want the most recent id
+  ;; If the user hasn't posted yet, start with the most recent global id
+  (or (cl-twit::get-newest-id (cl-twit:m-user-timeline))
+      (cl-twit::get-newest-id (cl-twit:m-public-timeline))))
+
 (defmethod initialize-instance :after ((bot microblog-follower-bot) &key)
   "Set up the bot's state."
   (with-microblog-user bot
     (when (= (most-recent-update-id bot) 0)
-      ;; Wasteful, as we only want the most recent id
-      ;; If the user hasn't posted yet, start with the most recent global id
       (setf (most-recent-update-id bot)
-	    (or (cl-twit::get-newest-id (cl-twit:m-user-timeline 
-					 :id (follow-id bot)))
-		(cl-twit::get-newest-id (cl-twit:m-public-timeline)))))))
+	    (current-user-posts-after-id)))))
+
+(defmethod response-for-message ((bot microblog-follower-bot) message)
+  "Response for the message object, or nil not to respond."
+  nil)
 
 (defmethod respond-to-message ((bot microblog-follower-bot) message)
   "Respond to the message object in some way."
-  nil)
+  (let ((response (response-for-message bot message)))
+    (when response
+      (post response))))
 
 (defmethod filter-messages ((bot microblog-follower-bot) messages)
   "Make sure only one mention from each user is listed."
@@ -235,8 +280,10 @@
   "Respond to new messages since messages were last processed."
   (assert (not (= (most-recent-update-id bot) 0)))
   (let ((messages 
-	 (cl-twit:m-user-timeline :id (follow-id bot) 
-				  :since-id (most-recent-update-id bot))))
+	 (or (ignore-errors
+	       (cl-twit:m-user-timeline :id (follow-id bot) 
+					:since-id (most-recent-update-id bot)))
+	     nil)))
     (when messages
       (dolist (message (filter-messages bot messages))
 	(respond-to-message bot message))
@@ -247,20 +294,3 @@
   "Run a single iteration of the bot main loop. For running as a cron job."
     (call-next-method)
     (respond-to-messages bot))
-
-#|
-
-(defclass trend-follower ()
-  ((connection)))
-
-(defclass search-follower ()
-  ((connection)
-   (words)))
-
-(defclass hashtag-follower (search-follower)
-  ())
-
-(defclass group-follower (search-follower)
-  ())
-
-|#
